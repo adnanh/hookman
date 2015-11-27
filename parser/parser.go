21ce2b3b958ec3d4a732c18eea4fabfe4384f735
Adnan Hajdarevic <adnanh@gmail.com>
2015-11-27 11:45:24 +0100

    scaffold

--- parser/parser.go
+++ parser/parser.go
@@ -1,1 +1,13 @@
 package parser
+
+import (
+	"github.com/adnanh/hookman/lexer"
+)
+
+type Parser struct {
+	Lexer *lexer.Lexer
+}
+
+func New(lexer *Lexer) *Parser {
+	return &Parser{Lexer: lexer}
+}
